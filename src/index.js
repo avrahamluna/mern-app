69b492c60beab9a8e0741e28ddaa60003c5232e8
Avraham Luna <avraham.luna@gmail.com>
2017-05-04 04:22:28 -0700

    creating React elements and JSX

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.render(
+    <h2 className="text-center">
+        Hello REACT with JSX!!</h2>,
+    document.getElementById('root')
+);
