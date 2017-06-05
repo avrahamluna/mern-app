ee1f84ba8755e0ebbbfbfcd0745bbf256e958f46
Avraham Luna <avraham.luna@gmail.com>
2017-06-05 00:50:23 -0700

    fetching data from a remote API

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
-import data from '../testData';
 
 class App extends React.Component {
     state = {
@@ -10,9 +10,14 @@ class App extends React.Component {
     };
 
 componentDidMount(){
-    this.setState({
-        contests:  data.contests
-    });
+    axios.get('/api/contests')
+    .then(resp =>{
+         this.setState({
+        contests:  resp.data.contests
+     });
+    })
+    .catch(console.error)
+   
 }
 
 
